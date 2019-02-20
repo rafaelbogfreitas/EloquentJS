607f51fb585fa9480f6831b8f9f66a247c2496b7
Rafael Freitas <rafaelborges1985@hotmail.com>
2019-02-20 15:47:02 +0000

    finished chapter 3

---
+++ Chapter3/BeanCounting.js
@@ -0,0 +1,20 @@
+function countBs(string){
+  let b = 0;
+  for(i=0;i<=string.length -1;i++){
+    if (string[i] == B) b = b + 1;
+  }
+  return b
+}
+
+function countChar(string, character){
+  let b = 0;
+  for (i=0;i<=string.length -1;i++){
+    if (string[i] == character) b = b + 1;
+  }
+  return b
+}
+
+console.log(countBs("BBC"));
+// → 2
+console.log(countChar("kakkerlak", "k"));
+// → 4

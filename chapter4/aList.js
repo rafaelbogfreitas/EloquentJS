cf06e43e0bff3e3a49d2a98621ec08eac2615c6d
Rafael Freitas <rafaelborges1985@hotmail.com>
2019-02-27 20:15:26 +0000

    adjusted aList.js

--- chapter4/aList.js
+++ chapter4/aList.js
@@ -1,11 +1,15 @@
+Write a function arrayToList that builds up a list structure like the one shown when given [1, 2, 3] as argument. Also write a listToArray function that produces an array from a list. Then add a helper function prepend, which takes an element and a list and creates a new list that adds the element to the front of the input list, and nth, which takes a list and a number and returns the element at the given position in the list (with zero referring to the first element) or undefined when there is no such element.
+
+If you haven’t already, also write a recursive version of nth.
+
 // Your code here.
 
 
 function arrayToList(array){
-  	let list = null;
-    for(i=array.length -1;i>=0;i--){
-		list = {value: array[i], rest : list};
-    }
+  let list = null;
+  for(i=array.length -1;i>=0;i--){
+    list = {value: array[i], rest : list};
+  }
   return list;
 };
 
@@ -29,7 +33,7 @@ function nth(list, position){
     counter++;
   }
 }
-console.log(nth());
+
 console.log(arrayToList([10, 20]));
 // → {value: 10, rest: {value: 20, rest: null}}
 console.log(listToArray(arrayToList([10, 20, 30])));
